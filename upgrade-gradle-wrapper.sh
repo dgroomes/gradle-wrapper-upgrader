7fd7b79ff5a779c723d538c295ba7b6daa6c35fe
David Groomes <dwgroomes@gmail.com>
2021-01-23 11:41:58 -0600

    Upgrade to Gradle 6.8.1

--- upgrade-gradle-wrapper.sh
+++ upgrade-gradle-wrapper.sh
@@ -5,7 +5,7 @@
 
 # Latest Gradle releases: https://gradle.org/releases/
 # Set the version of Gradle to upgrade to (overridable via environment variable)
-: ${GRADLE_VERSION:=6.7.1}
+: ${GRADLE_VERSION:=6.81.1}
 
 upgradeWrapperInDirectory() {
 
