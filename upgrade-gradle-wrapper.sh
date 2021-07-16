0e8bc09fbf20ccfc3856c218be45aee958ee48eb
David Groomes <dwgroomes@gmail.com>
2021-07-16 18:04:27 -0500

    Upgrade to Gradle 7.1.1

--- upgrade-gradle-wrapper.sh
+++ upgrade-gradle-wrapper.sh
@@ -5,7 +5,7 @@
 
 # Latest Gradle releases: https://gradle.org/releases/
 # Set the version of Gradle to upgrade to (overridable via environment variable)
-: ${GRADLE_VERSION:=7.1}
+: ${GRADLE_VERSION:=7.1.1}
 
 upgradeWrapperInDirectory() {
 
